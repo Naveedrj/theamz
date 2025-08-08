b4d292fe56c8a53bb61ecd2c59d7b0cc0ccd14ec
Naveed Rajput <naveed@naveeds-MacBook-Pro.local>
2025-08-08 22:10:06 +0500

    issue

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // firebase.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./theamzresearch-3dbf0-firebase-adminsdk-fbsvc-b1f315d4fb.json'); // secure file
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT); // secure file
 
 if (!admin.apps.length) {
   admin.initializeApp({
