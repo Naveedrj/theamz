52143bc3e29371784a0a8c12f4b5554873836a39
Naveed Rajput <naveed@naveeds-MacBook-Pro.local>
2025-08-08 22:15:46 +0500

    issue

--- firebase.js
+++ firebase.js
@@ -1,11 +1,7 @@
-// firebase.js
 const admin = require('firebase-admin');
-const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT); // secure file
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
-const db = admin.firestore();
-module.exports = { admin, db };
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
